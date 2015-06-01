77ab5f49524ddbef63df165edfa4046ff318165d
Ziosa <ziosa84@gmail.com>
2015-06-02 00:41:02 +0200

    add total of price in the cart

--- app/components/app.js
+++ app/components/app.js
@@ -4,23 +4,35 @@ import ItemList from './itemList';
 
 const Cart = React.createClass({
   render() {
-      let createItem = function(itemText) {
+      let totalCount = 0;
+      let that = this;
+      let createItem = function(item) {
         return (
-          <li><h2>Name: {itemText.title} Price:{itemText.price}</h2></li>
+          <li><h2>Name: {item.title} Price:{item.price}</h2></li>
           );
       };
-      return <div><h1>Cart:</h1><ul>{this.props.items.map(createItem)}</ul></div>;
+      return(<div>
+                <h1>Cart:</h1>
+                <ul>{this.props.items.map(createItem)}</ul>
+                <h2>Total: {this.props.totalPrice}</h2>
+              </div>
+            );
   }
 });
 
 const App = React.createClass({
   getInitialState: function() {
-       return {itemsInCart: []};
+       return {
+         itemsInCart: [],
+         totalPrice : 0
+         };
   },
   addToCart: function(item) {
     let allItems = this.state.itemsInCart.concat([item]);
-        this.setState({
-          itemsInCart: allItems
+    let tot = this.state.totalPrice;
+    this.setState({
+      itemsInCart : allItems,
+      totalPrice : tot+=item.price
     });
   },
   render() {
@@ -30,7 +42,7 @@ const App = React.createClass({
             addToCart={this.addToCart}
             items={this.props.items}
           />
-          <Cart items={this.state.itemsInCart}/>
+        <Cart items={this.state.itemsInCart} totalPrice={this.state.totalPrice}/>
         </div>
     );
   }
